bc1d148fb20070416d2fef5feaa3668aff004824
Nick <nick.anthony@eco-surv.co.uk>
2019-07-22 11:27:48 +0100

    1.1.7

---
+++ CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+All notable changes to `laravel-mongodb-passport` will be documented in this file
+
+## 1.1.7 - 2018-08-13
+- removed incorrect laravel/passport "dev*" string
+
+## 1.1.6 - 2018-08-13
+- added support for new jenseggers/laravel-mongodb version "3.5.X"
+
+## 1.1.5 - 2018-08-13
+- don't cast scopes as an array
+
+## 1.1.4 - 2018-08-13
+- add support for passport 7
+
+## 1.1.3 - 2018-04-24
+- add support for passport 6
+
+## 1.1.2 - 2018-03-22
+- fix support for laravel-framework 5.6, laravel-passport 5, laravel-mongodb 3.4
+
+## 1.1.1 - 2018-01-22
+- fix lumen support
+
+## 1.1.0 - 2018-01-11
+- transfer ownership to narwy
+
+## 1.0.1 - 2018-01-11
+- add friendly User class implementation
+
+## 1.0.0 - 2018-01-10
+- initial release
---
+++ src/MongodbPassportServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Narwy\Mongodb;
+
+use Illuminate\Support\ServiceProvider;
+use Narwy\Mongodb\Passport\AuthCode;
+use Narwy\Mongodb\Passport\Client;
+use Narwy\Mongodb\Passport\PersonalAccessClient;
+use Narwy\Mongodb\Passport\Token;
+
+class MongodbPassportServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        /*
+         * Passport client extends Eloquent model by default, so we alias them.
+         */
+        if (class_exists('Illuminate\Foundation\AliasLoader')) {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
+            $loader->alias('Laravel\Passport\Client', Client::class);
+            $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+            $loader->alias('Laravel\Passport\Token', Token::class);
+        } else {
+            class_alias('Laravel\Passport\AuthCode', AuthCode::class);
+            class_alias('Laravel\Passport\Client', Client::class);
+            class_alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
+            class_alias('Laravel\Passport\Token', Token::class);
+        }
+    }
+}
