d77ae14f0a156da5d915554e74d4a40838bccc68
narwy <nick@t3am.io>
2019-10-01 15:43:53 +0100

    Support for Laravel v6

--- src/MongodbPassportServiceProvider.php
+++ src/MongodbPassportServiceProvider.php
@@ -7,6 +7,8 @@ use Narwy\Mongodb\Passport\AuthCode;
 use Narwy\Mongodb\Passport\Client;
 use Narwy\Mongodb\Passport\PersonalAccessClient;
 use Narwy\Mongodb\Passport\Token;
+use Narwy\Mongodb\Passport\Bridge\RefreshTokenRepository;
+use Narwy\Mongodb\Passport\Bridge\RefreshToken;
 
 class MongodbPassportServiceProvider extends ServiceProvider
 {
@@ -19,11 +21,15 @@ class MongodbPassportServiceProvider extends ServiceProvider
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('Laravel\Passport\AuthCode', AuthCode::class);
             $loader->alias('Laravel\Passport\Client', Client::class);
+            $loader->alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+            $loader->alias('Laravel\Passport\Bridge\RefreshToken', RefreshToken::class);
             $loader->alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
             $loader->alias('Laravel\Passport\Token', Token::class);
         } else {
             class_alias('Laravel\Passport\AuthCode', AuthCode::class);
             class_alias('Laravel\Passport\Client', Client::class);
+            class_alias('Laravel\Passport\Bridge\RefreshTokenRepository', RefreshTokenRepository::class);
+            class_alias('Laravel\Passport\Bridge\RefreshToken', RefreshToken::class);
             class_alias('Laravel\Passport\PersonalAccessClient', PersonalAccessClient::class);
             class_alias('Laravel\Passport\Token', Token::class);
         }
